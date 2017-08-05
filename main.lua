ad1f7fb5b912a4b6ce0e5f73a87acb6421f1e83e
KayleMaster <evgenit28@gmail.com>
2017-08-05 17:24:38 +0300

    Created repo.

---
+++ main.lua
@@ -0,0 +1,67 @@
+
+
+
+function love.load()
+
+	--Version, title and window information
+	    version = 0.001;
+	    width, height, flags = love.window.getMode();
+	    love.window.setTitle( "Stronghold Empires ".."v"..version);
+	    image = love.graphics.newImage( "assets/tiles/collection148.png" )
+	    view_xview = 0;
+	    view_yview = 0;
+	    mx = 0;
+	    my = 0;
+	    LocalX = 0;
+	    LocalY = 0;
+
+	--Request other files
+		require("terrain");
+		setup_terrain();
+end
+
+
+
+
+function love.update(dt)
+  if love.keyboard.isDown("up")  then
+    view_yview = view_yview-5;  
+  end
+  if love.keyboard.isDown("down")  then
+    view_yview = view_yview+5;  
+  end
+  if love.keyboard.isDown("left")  then
+    view_xview = view_xview -5;  
+  end
+  if love.keyboard.isDown("right")  then
+    view_xview = view_xview +5;  
+  end
+
+  if love.keyboard.isDown("up") or love.keyboard.isDown("down") or love.keyboard.isDown("left") or love.keyboard.isDown("right") then
+  	update_terrain();
+  end
+
+  mx, my = love.mouse.getPosition( )  
+   LocalX = math.round(ScreenToIsoX(mx-16+view_xview, my-8+view_yview)); LocalY = math.round(ScreenToIsoY(mx-16+view_xview, my-8+view_yview)); 
+
+end
+
+
+
+
+
+function love.draw()
+
+    draw_terrain();
+    love.graphics.draw(image,IsoToScreenX(LocalX,LocalY)-view_xview,IsoToScreenY(LocalX,LocalY)-view_yview)
+
+
+
+        love.graphics.print("v"..terrain_chunk[1][1] ..
+         "\n LocalX: " .. LocalX ..
+         "\n LocalY: " .. LocalY ..
+         "\n view_xview: " .. view_xview ..
+         "\n view_yview: " .. view_yview ..
+         "\nCurrent FPS: "..tostring(love.timer.getFPS( )), 0, 0);
+ 
+end
---
+++ terrain.lua
@@ -0,0 +1,99 @@
+function setup_terrain()
+	--Terrain Initialize
+	----Tiles
+	        tile_width = 32;
+	        tile_height = 16;
+	        tile_variations = 1;
+
+    ----Chunks
+	        chunk_width = 32;
+	        chunk_height = 32;
+	        chunk_size = chunk_width*chunk_height;
+	        IsoX = 400;
+            IsoY = 0;
+	----Rows and columns
+            cols = chunk_width;
+            rows = chunk_height;
+	----Chunk 2D array
+	        terrain_chunk = {}          
+
+            for y = 0,cols do
+                local row = {}
+                    for x = 0,rows do
+                        row[x]=love.math.random(8);
+                    end
+                terrain_chunk[y]=row;
+            end
+            array_x = 0;
+            array_y = 0;
+
+	----Generate spriteBatch
+	        terrain_image = love.graphics.newImage( "assets/tiles/terrain_strip2.png" );
+	        tile_quads = {};
+	        tile_quads[1] = love.graphics.newQuad(0 * tile_width, 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        tile_quads[2] = love.graphics.newQuad(1 * (tile_width-2), 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        tile_quads[3] = love.graphics.newQuad(2 * (tile_width-2), 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        tile_quads[4] = love.graphics.newQuad(3 * (tile_width-2), 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        tile_quads[5] = love.graphics.newQuad(4 * (tile_width-2), 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        tile_quads[6] = love.graphics.newQuad(5 * (tile_width-2), 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        tile_quads[7] = love.graphics.newQuad(6 * (tile_width-2), 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        tile_quads[8] = love.graphics.newQuad(7 * (tile_width-2), 0 * tile_height, tile_width-2, tile_height, terrain_image:getWidth(), terrain_image:getHeight())
+	        terrain_batch = love.graphics.newSpriteBatch(terrain_image, chunk_width*chunk_height)
+	----Generate terrain    
+            update_terrain();
+end
+
+
+function update_terrain()
+  terrain_batch:clear()
+  for i=chunk_width-1,0,-1 do
+    for o=chunk_height-1,0,-1 do
+      terrain_batch:add(
+		  				tile_quads[terrain_chunk[i][o]], 
+      					(-view_xview)+IsoX + (i - o) * tile_width  * 0.5,
+      					(-view_yview)+IsoY + (i + o) * tile_height * 0.5
+						  )
+    end
+  end
+  terrain_batch:flush()
+end
+
+function draw_terrain()
+  love.graphics.draw(terrain_batch,    0, 0, 0, 1, 1)
+end
+
+function math.round(n, deci) deci = 10^(deci or 0) return math.floor(n*deci+.5)/deci end
+
+
+function ScreenToIsoX(globalX, globalY) 
+    return (((globalX - IsoX) / (tile_width/2)) + ((globalY - IsoY) / (tile_height/2))) / 2;
+end
+
+function ScreenToIsoY(globalX, globalY) 
+    return (((globalY - IsoY) / (tile_height/2)) - ((globalX - IsoX) / (tile_width/2))) / 2;
+end
+
+function IsoToScreenX(xx, yy) 
+    return IsoX + ((xx - yy) * tile_width/2); end
+
+function IsoToScreenY(xx, yy) 
+    return IsoY + ((xx + yy) * tile_height/2); end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
