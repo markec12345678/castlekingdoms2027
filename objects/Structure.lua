65b118fd49bbced662568f39a903f464a449f893
KayleMaster <evgenit28@gmail.com>
2022-02-02 22:33:09 +0200

    Fix building animations not affected by elevation
    Closes #47

--- objects/Structure.lua
+++ objects/Structure.lua
@@ -23,6 +23,11 @@ function Structure:animate(dt, force_update)
         local instancemesh = object_mesh[self.cx][self.cy]
         local quad, x, y, _, _, _, _, _, _, _ = self.animation:getFrameInfo(self.x + (self.offset_x or 0) + offset_x,
             self.y + (self.offset_y or 0) + offset_y - _G.height_map[self.gx][self.gy])
+        local elevation_offset_y = 0
+        if _G.heightmap[self.cx][self.cy][self.i][self.o] then
+            elevation_offset_y = _G.heightmap[self.cx][self.cy][self.i][self.o] * 2
+        end
+        y = y - elevation_offset_y
         local qx, qy, qw, qh = quad:getViewport()
         self.vert_id = _G.getFreeVertexFromTile(self.cx, self.cy, self.i, self.o)
         if self.vert_id then
@@ -39,6 +44,11 @@ function Structure:animate(dt, force_update)
         end
         local quad, x, y, _, _, _, _, _, _, _ = self.animation:getFrameInfo(self.x + (self.offset_x or 0) + offset_x,
             self.y + (self.offset_y or 0) + offset_y - _G.height_map[self.gx][self.gy])
+        local elevation_offset_y = 0
+        if _G.heightmap[self.cx][self.cy][self.i][self.o] then
+            elevation_offset_y = _G.heightmap[self.cx][self.cy][self.i][self.o] * 2
+        end
+        y = y - elevation_offset_y
         if quad then
             local qx, qy, qw, qh = quad:getViewport()
             self.instancemesh:setVertex(self.vert_id, x, y, qx, qy, qw, qh, 1)
