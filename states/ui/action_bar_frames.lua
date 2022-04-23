2c2130b2fe78d475ada3369b570b070091885466
KayleMaster <evgenit28@gmail.com>
2022-04-23 18:53:35 +0300

    Refactor action_bar_frames to be DRY

--- states/ui/action_bar_frames.lua
+++ states/ui/action_bar_frames.lua
@@ -7,6 +7,7 @@ local ACTION_BAR_HEIGHT = 193 - ACTION_BAR_Y_OFFSET
 local ACTION_BUTTON_WIDTH = 299 - ACTION_BAR_X_OFFSET
 local ACTION_BUTTON_SPACING = 3
 local scale = action_bar.scalex
+local AB_INSIDE_PADDING = 7 * scale
 
 local fr_full = {
     x = action_bar:GetX() - action_bar:GetOffsetX() * scale,
@@ -22,202 +23,24 @@ local fr_action_bar = {
     width = ACTION_BAR_WIDTH * scale,
     height = ACTION_BAR_HEIGHT * scale
 }
-
-local fr_action_1 = {
-    x = fr_action_bar.x,
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_2 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (2 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_3 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (3 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_4 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (4 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_5 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (5 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_6 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (6 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_7 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (7 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_8 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (8 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_9 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (9 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_10 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (10 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_11 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (11 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local fr_action_12 = {
-    x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (12 - 1),
-    y = fr_action_bar.y,
-    width = ACTION_BUTTON_WIDTH * scale,
-    height = ACTION_BAR_HEIGHT * scale
-}
-
-local AB_INSIDE_PADDING = 7 * fr_full.scale
-
-local fr_action_1_img = {
-    x = fr_action_1.x + AB_INSIDE_PADDING,
-    y = fr_action_1.y + AB_INSIDE_PADDING,
-    width = fr_action_1.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_1.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_2_img = {
-    x = fr_action_2.x + AB_INSIDE_PADDING,
-    y = fr_action_2.y + AB_INSIDE_PADDING,
-    width = fr_action_2.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_2.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_3_img = {
-    x = fr_action_3.x + AB_INSIDE_PADDING,
-    y = fr_action_3.y + AB_INSIDE_PADDING,
-    width = fr_action_3.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_3.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_4_img = {
-    x = fr_action_4.x + AB_INSIDE_PADDING,
-    y = fr_action_4.y + AB_INSIDE_PADDING,
-    width = fr_action_4.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_4.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_5_img = {
-    x = fr_action_5.x + AB_INSIDE_PADDING,
-    y = fr_action_5.y + AB_INSIDE_PADDING,
-    width = fr_action_5.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_5.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_6_img = {
-    x = fr_action_6.x + AB_INSIDE_PADDING,
-    y = fr_action_6.y + AB_INSIDE_PADDING,
-    width = fr_action_6.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_6.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_7_img = {
-    x = fr_action_7.x + AB_INSIDE_PADDING,
-    y = fr_action_7.y + AB_INSIDE_PADDING,
-    width = fr_action_7.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_7.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_8_img = {
-    x = fr_action_8.x + AB_INSIDE_PADDING,
-    y = fr_action_8.y + AB_INSIDE_PADDING,
-    width = fr_action_8.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_8.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_9_img = {
-    x = fr_action_9.x + AB_INSIDE_PADDING,
-    y = fr_action_9.y + AB_INSIDE_PADDING,
-    width = fr_action_9.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_9.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_10_img = {
-    x = fr_action_10.x + AB_INSIDE_PADDING,
-    y = fr_action_10.y + AB_INSIDE_PADDING,
-    width = fr_action_10.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_10.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_11_img = {
-    x = fr_action_11.x + AB_INSIDE_PADDING,
-    y = fr_action_11.y + AB_INSIDE_PADDING,
-    width = fr_action_11.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_11.height - AB_INSIDE_PADDING * 2
-}
-
-local fr_action_12_img = {
-    x = fr_action_12.x + AB_INSIDE_PADDING,
-    y = fr_action_12.y + AB_INSIDE_PADDING,
-    width = fr_action_12.width - AB_INSIDE_PADDING * 2,
-    height = fr_action_12.height - AB_INSIDE_PADDING * 2
-}
-
-return {
+local frames = {
     fr_full = fr_full,
-    fr_action_bar = fr_action_bar,
-    fr_action_1 = fr_action_1,
-    fr_action_2 = fr_action_2,
-    fr_action_3 = fr_action_3,
-    fr_action_4 = fr_action_4,
-    fr_action_5 = fr_action_5,
-    fr_action_6 = fr_action_6,
-    fr_action_7 = fr_action_7,
-    fr_action_8 = fr_action_8,
-    fr_action_9 = fr_action_9,
-    fr_action_10 = fr_action_10,
-    fr_action_11 = fr_action_11,
-    fr_action_12 = fr_action_12,
-    fr_action_1_img = fr_action_1_img,
-    fr_action_2_img = fr_action_2_img,
-    fr_action_3_img = fr_action_3_img,
-    fr_action_4_img = fr_action_4_img,
-    fr_action_5_img = fr_action_5_img,
-    fr_action_6_img = fr_action_6_img,
-    fr_action_7_img = fr_action_7_img,
-    fr_action_8_img = fr_action_8_img,
-    fr_action_9_img = fr_action_9_img,
-    fr_action_10_img = fr_action_10_img,
-    fr_action_11_img = fr_action_11_img,
-    fr_action_12_img = fr_action_12_img
+    fr_action_bar = fr_action_bar
 }
+for i = 1, 12 do
+    local fr_action_obj = {
+        x = fr_action_bar.x + (ACTION_BUTTON_WIDTH + ACTION_BUTTON_SPACING) * scale * (i - 1),
+        y = fr_action_bar.y,
+        width = ACTION_BUTTON_WIDTH * scale,
+        height = ACTION_BAR_HEIGHT * scale
+    }
+    frames["fr_action_" .. i .. "_img"] = {
+        x = fr_action_obj.x + AB_INSIDE_PADDING,
+        y = fr_action_obj.y + AB_INSIDE_PADDING,
+        width = fr_action_obj.width - AB_INSIDE_PADDING * 2,
+        height = fr_action_obj.height - AB_INSIDE_PADDING * 2
+    }
+    frames["fr_action_" .. i] = fr_action_obj
+end
+
+return frames
