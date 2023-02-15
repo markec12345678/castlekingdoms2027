dfc88734c11ab47a1c5ff41a69ddfa21478de69f
Kayle <1516023-kaylemaster@users.noreply.gitlab.com>
2023-02-15 06:45:46 +0000

    Fix unit test pipeline

--- spec/love-mocks.lua
+++ spec/love-mocks.lua
@@ -1,6 +1,25 @@
 -- mocks for LÖVE functions
 local unpack = _G.unpack or table.unpack
 
+local font = {
+    getAscent = function() return 10 end,
+    getBaseline = function() return 10 end,
+    getDPIScale = function() return 1 end,
+    getDescent = function() return 10 end,
+    getFilter = function() return end,
+    getHeight = function() return 1000 end,
+    getKerning = function() return end,
+    getLineHeight = function() return 10 end,
+    getWidth = function() return 2000 end,
+    getWrap = function() return true end,
+    hasGlyphs = function() return false end,
+    setFallbacks = function(a, ...) return end,
+    setFilter = function(a, b, c) return end,
+    setLineHeight = function(x) return end,
+    getDimensions = function() return 6000, 6000 end,
+    setWrap = function() return end,
+}
+
 local Quadmt = {
     __eq = function(a, b)
         if #a ~= #b then
@@ -26,15 +45,126 @@ local Quadmt = {
 }
 
 Quadmt.__index = Quadmt
-
-_G.love = {
-    graphics = {
-        newQuad = function(...)
-            return setmetatable({...}, Quadmt)
-        end,
-        draw = function()
-        end,
-        getLastDrawq = function()
-        end
-    }
+love.graphics = {
+    clear = function() end,
+    draw = function() end,
+    flushBatch = function() end,
+    newQuad = function(...)
+        return setmetatable({...}, Quadmt)
+    end,
+    newImage = function(filename)
+        if type(filename) ~= "string" then error("love.graphics.newImage expects a string, but got a " .. filename) end
+        local fileExists = love.filesystem.getInfo(filename)
+        if not fileExists then error("trying to load file: " .. tostring(filename) .. " but it doesn't exist") end
+        return font
+    end,
+    newShader = function() return {} end,
+    newFont = function() return font end,
+    newImageFont = function() return font end,
+    getLastDrawq = function() end,
+    setDefaultFilter = function() end,
+    reset = function() end,
+    push = function() end,
+    translate = function() end,
+    setBackgroundColor = function() end,
+    pop = function() end,
+    scale = function() end,
+    isActive = function() return false end,
+    newMesh = function() return {
+            attachAttribute = function() end,
+            detachAttribute = function() end,
+            flush = function() end,
+            getDrawMode = function() end,
+            getDrawRange = function() end,
+            getImage = function() end,
+            getTexture = function() end,
+            getVertex = function() end,
+            getVertexAttribute = function() end,
+            getVertexCount = function() end,
+            getVertexFormat = function() end,
+            getVertexMap = function() end,
+            getVertices = function() end,
+            hasVertexColors = function() end,
+            isAttributeEnabled = function() end,
+            setAttributeEnabled = function() end,
+            setDrawMode = function() end,
+            setDrawRange = function() end,
+            setImage = function() end,
+            setTexture = function() end,
+            setVertex = function() end,
+            setVertexAttribute = function() end,
+            setVertexColors = function() end,
+            setVertexMap = function() end,
+            setVertices = function() end,
+        }
+    end,
+    newSpriteBatch = function()
+        return {
+            add = function(a, b, c, d, e, f) end,
+            attachAttribute = function(a, b, c, d, e, f) end,
+            clear = function(a, b, c, d, e, f) end,
+            flush = function(a, b, c, d, e, f) end,
+            getBufferSize = function(a, b, c, d, e, f) end,
+            getCount = function(a, b, c, d, e, f) end,
+            getImage = function(a, b, c, d, e, f) end,
+            getTexture = function(a, b, c, d, e, f) end,
+            set = function(a, b, c, d, e, f) end,
+            setColor = function(a, b, c, d, e, f) end,
+            setImage = function(a, b, c, d, e, f) end,
+            setTexture = function(a, b, c, d, e, f) end,
+        }
+    end,
+    print = function() end,
+    printf = function() end,
+    getHeight = function() return 1920 end,
+    getWidth = function() return 1080 end,
+    getDimensions = function() return 1920, 1080 end,
 }
+
+love.window = {
+    getMode = function() return 1920, 1080, true end,
+    setFullscreen = function() return end,
+}
+
+love.audio = {
+    getActiveEffects = function() return {} end,
+    getActiveSourceCount = function() return 0 end,
+    getDistanceModel = function() return {} end,
+    getDopplerScale = function() return 1 end,
+    getEffect = function() end,
+    getMaxSceneEffects = function() end,
+    getMaxSourceEffects = function() end,
+    getNumSources = function() end,
+    getOrientation = function() end,
+    getPosition = function() end,
+    getRecordingDevices = function() return 0 end,
+    getSourceCount = function() return 1 end,
+    getVelocity = function() end,
+    getVolume = function() return 100 end,
+    isEffectsSupported = function() end,
+    newQueueableSource = function() end,
+    newSource = function() return {
+            setVolumeLimits = function(a) end,
+            play = function() end,
+            setRelative = function(a) end,
+            setPosition = function(a) end,
+            setPitch = function(a) end,
+            setVolume = function(a) end,
+        }
+    end,
+    pause = function() end,
+    play = function() end,
+    resume = function() end,
+    rewind = function() end,
+    setDistanceModel = function() end,
+    setDopplerScale = function() end,
+    setEffect = function() end,
+    setMixWithSystem = function() end,
+    setOrientation = function() end,
+    setPosition = function() end,
+    setVelocity = function() end,
+    setVolume = function() end,
+}
+
+love.mouse.newCursor = function(a, b, c) return {} end
+love.mouse.setCursor = function(a) end
--- states/test.lua
+++ states/test.lua
@@ -1,7 +1,73 @@
 local test = {}
-local thread
+local loveframes = require("libraries.loveframes")
+local ActionBar = require("states.ui.ActionBar")
+local states = require("states.ui.states")
+local core = require("misc")
+local thread, thread2, objects, terrain
+local SaveManager = require("objects.Controllers.SaveManager")
+local savegame
+
+_G.dt = 0.016
 
 function test:enter()
+    local State = require("objects.State")
+    _G.state = State:new()
+    objects = love.filesystem.load("objects/objects.lua")(objectAtlas)
+    package.loaded["objects.objects"] = objects
+    terrain = require("terrain.terrain")
+    _G.BrushController = require("objects.Controllers.BrushController")
+    _G.DestructionController = require("objects.Controllers.DestructionController"):new()
+    RationController = require("objects.Controllers.RationController")
+    _G.TaxController = require("objects.Controllers.TaxController")
+    _G.TimeController = require("objects.Controllers.TimeController")
+    _G.PopularityController = require("objects.Controllers.PopularityController")
+    _G.ScribeController = require("objects.Controllers.ScribeController")
+    _G.BuildController = love.filesystem.load("objects/Controllers/BuildController.lua")(
+        package.loaded["objects.objects"].object, objectAtlas)
+    _G.JobController = require("objects.Controllers.JobController")
+    _G.BuildingManager = require("objects.Controllers.BuildingManager")
+    _G.DebugView = require("objects.Controllers.DebugView")
+    ----Pathfinding setup
+    thread = love.thread.newThread("libraries/pathfinding_thread.lua")
+    thread:start("1")
+    thread2 = love.thread.newThread("libraries/pathfinding_thread.lua")
+    thread2:start("2")
+    _G.finder = require("objects.Controllers.PathController")
+    _G.state.newGame = savegame == "map_Fernhaven"
+    for cx = 0, _G.chunksWide - 1 do
+        for cy = 0, _G.chunksHigh - 1 do
+            _G.allocateMesh(cx, cy)
+        end
+    end
+    if _G.state.newGame then
+        SaveManager:load(savegame)
+
+        _G.BuildController:set("SaxonHall")
+    else
+
+        SaveManager:load(savegame)
+    end
+    core.update()
+    objects.update(_G.dt)
+    _G.BuildController:update()
+    loveframes.update()
+    _G.finder:update()
+    love.timer.sleep(0.4)
+    local error = thread:getError()
+    assert(not error, error)
+    loveframes.SetState(states.STATE_INGAME_CONSTRUCTION)
+    ActionBar:updateGoldCount()
+    ActionBar:updatePopularityCount()
+    _G.loaded = true
+    if _G.state.newGame then
+        _G.playSpeech("place_a_keep")
+    end
+
+
+
+
+    local State = require("objects.State")
+    _G.state = State:new()
     require('spec.objects_spec')
     _G.BuildController:set('castle')
     _G.BuildController.start = true
