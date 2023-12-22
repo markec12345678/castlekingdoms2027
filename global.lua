39c516cd47ae2bf347b9f0224fd91103dd878420
KayleMaster <evgenit28@gmail.com>
2023-12-22 12:25:57 +0200

    Bump version to 0.6.0-dev

--- global.lua
+++ global.lua
@@ -10,7 +10,7 @@ _G.OPTIONS = {
     MUSIC_VOLUME = config.sound.music / 100
 }
 
-_G.version = "0.5.0"
+_G.version = "0.6.0-dev"
 _G.classes = {}
 _G.anim = require("libraries.anim8")
 _G.class = require("libraries.middleclass").class
